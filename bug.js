18f75c6d7123f280a7b992a27b12be6250dda1a5
BugHunter-X <askzenai@gmail.com>
2025-02-23 01:34:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+function myFunction(a, b) {
+  return a + b;
+}
+
+console.log(myFunction(5, 10)); // Output: 15
+console.log(myFunction(5, '10')); // Output: 510
+console.log(myFunction(true, 1)); //Output: 1true
